45e3286551da33c20c95ede8b0999aabc80f322f
RikuTanooka <ht21a065@oecu.jp>
2023-07-24 16:40:10 +0900

    設定ファイルの記述修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,28 @@
 module.exports = {
-    env: {
+  env: {
       browser: true,
-      es2021: true,
-    },
-    extends: [
-      'airbnb',
-      'airbnb-typescript',
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
+      es2022: true,
+  },
+  extends: [
+      "airbnb",
+      "airbnb-typescript",
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
       ecmaFeatures: {
-        jsx: true,
+          jsx: true,
       },
       ecmaVersion: 13,
-      sourceType: 'module',
-      project: ['./tsconfig.json', '.eslintrc.js'],
-    },
-    plugins: ['react', '@typescript-eslint'],
-    rules: {
-      'no-console': 'off',
-      'no-var': '0',
-    },
-  }
-  
+      sourceType: "module",
+      project: ["./tsconfig.json"],
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+      "no-console": "off",
+      "no-var": "0",
+  },
+};
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,23 +1,34 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-    printWidth: 120,
-    tabWidth: 4,
-    useTabs: false,
-    semi: true,
-    singleQuote: false,
-    quoteProps: 'consistent',
-    jsxSingleQuote: false,
-    trailingComma: 'all',
-    bracketSpacing: true,
-    bracketSameLine: false,
-    arrowParens: 'always',
-    overrides: [
+  printWidth: 120,
+  tabWidth: 4,
+  //useTabs: false,
+  //semi: true,
+  //singleQuote: false,
+  quoteProps: "consistent",
+  //jsxSingleQuote: false,
+  trailingComma: "es5",
+  //bracketSpacing: true,
+  //bracketSameLine: false,
+  //arrowParens: 'always',
+  //parser: "typescript",
+  //filepath: "",
+  //requirePragma: true,
+  //insertPragma: true,
+  //proseWrap: "always",
+  //htmlWhitespaceSensitivity: CSS,
+  //vueIndentScriptAndStyle: false,
+  //endOfLine: "auto",
+  //embeddedLanguageFormatting: "auto",
+  singleAttributePerLine: true,
+  /*
+  overrides: [
       {
-        files: '*.ts',
-        options: {
-          parser: 'typescript',
-        },
+          files: "*.ts",
+          options: {
+              parser: "typescript",
+          },
       },
-    ],
-  }
-  
+  ],
+  */
+};
